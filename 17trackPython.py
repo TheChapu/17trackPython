6d441d22abc47d09d203c585e86a55b612be09be
TheChapu <yo@hacking.cl>
2017-03-27 12:33:42 -0300

    Update 17trackPython.py
    Funcionando

--- 17trackPython.py
+++ 17trackPython.py
@@ -3,36 +3,49 @@
 #Cuentan con una Api pero es PAGADA
 
 #Codigo para obtener informacion de envios ---> 17Track
-#Agradecimientos a @jsavargas
+#Agradecimientos a @unkndown
 
 # Importamos las librerias
-#from bs4 import BeautifulSoup
 import requests
 import urllib
 import httplib
 import json
-#RH352992401CN
-#ALS00359081
-# Pagina
 
-ID = "RH352992401CNT"#Aqui numero de seguimiento
+ID = "CN750400915LT"#Aqui numero de seguimiento
 
 host = "17track.net"
 link = "http://www.17track.net/restapi/handlertrack.ashx"
-parametros = urllib.urlencode({"guid":"","data":[{"num":"RH352992401CNT"}]})
+parametros = '{"guid":"","data":[{"num":"'+ID+'"}]}'
+
 headers    = {
 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
 "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
 "Accept": "*/*",
-"Referer": "http://www.17track.net/es/track?nums=RH352992401CNT&fc=0",
+"Referer": "http://www.17track.net/es/track?nums="+ID+"&fc=0",
 'X-Requested-With': 'XMLHttpRequest',
 "Cookie":"__cfduid=d068db605f07408be5e77b46a1b5a3c9b1489755836; __atuvc=5%7C11; __atuvs=58cbdebee36d7721004; v5_ShowedWelcome=1; _ga=GA1.2.102662854.1489755840; __gads=ID=59a7a3123039d55c:T=1489755840:S=ALNI_MaM_SfNKpkXhENkfjaxZbBDSQ5txQ; v5_TrackingGuid=04998d4cd8844d089ba29484654e257a; _gat=1"
 }
 
+
 conexion   = httplib.HTTPConnection(host)
 conexion.request("POST", link, parametros, headers)
 respuesta  = conexion.getresponse()
-#ver_source = respuesta.read()
-print respuesta.status,respuesta.read()
+ver_source = respuesta.read()
 
-#CONERRORES
+data = json.loads(ver_source)
+
+print "Informacion Chile\n"
+for i in range (0,len(data['dat'][0]['track']['z2'])):
+	print data['dat'][0]['track']['z2'][i]['a']
+	print data['dat'][0]['track']['z2'][i]['c']
+	print data['dat'][0]['track']['z2'][i]['z']
+	print
+
+print"******************************************"
+
+print "Informacion Extranjero\n"
+for i in range (0,len(data['dat'][0]['track']['z1'])):
+	print data['dat'][0]['track']['z1'][i]['a']
+	print data['dat'][0]['track']['z1'][i]['c']
+	print data['dat'][0]['track']['z1'][i]['z']
+	print
